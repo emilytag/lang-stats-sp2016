c78b30b0626655987be009ae7baba8c7e8a9f558
elliotschumacher@gmail.com <elliotschumacher@gmail.com>
2016-04-22 20:31:42 -0400

    Adding additional features.

--- classifier.py
+++ classifier.py
@@ -4,6 +4,16 @@ from sklearn.feature_extraction import DictVectorizer
 import sys
 from syntax import trainSyntax, devtestSyntax
 import subprocess
+from sklearn.feature_selection.from_model import SelectFromModel
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.feature_selection import RFECV
+from sklearn.linear_model.randomized_l1 import RandomizedLogisticRegression
+from sklearn.ensemble.forest import RandomForestClassifier
+import datetime
+import pickle
+import nltk
 
 class LogRegModel:
     def __init__(self):
@@ -27,7 +37,9 @@ class LogRegModel:
       featureSet.update(feats)
       #temp_output_file = open('test_sent.txt', "w")
       #temp_output_file.write(article)
-      #subprocess.call("ngram/ngram -ppl " + 'test_sent.txt' + " -order 4 -lm ngram/data_train.5.lm")
+      #subprocess.call("ngram/ngram -ppl " + 'test_sent.txt' + " -order 4 -lm ngram/data_train.5.lm"
+      text = word_tokenize("And now for something completely different")
+      nltk.pos_tag(text)
       return featureSet
 
     def learn(self, article, classification, feats):
@@ -36,16 +48,37 @@ class LogRegModel:
       self.allCorrect.append(classification)
 
     def fitModel(self):
-      X = self.vec.fit_transform(self.allFeatures).toarray()
-      y = np.array(self.allCorrect)
+      X = self.vec.fit_transform(self.allFeatures).toarray().astype(np.float)
+      y = np.array(self.allCorrect).astype(np.float)
+      self.featSelect = SelectFromModel(RandomForestClassifier()).fit(X,y)
+      #lr = 
+      #self.featSelect = RandomizedLogisticRegression().fit(X,y)#SelectFromModel(lr,prefit=True
+      X = self.featSelect.transform(X)
+      print(X.shape)
+      self.printSelectedFeats()
+      usePreloaded = True
+      if (usePreloaded):
+          featSelectFilename = "featselect_{0}.pkl".format(datetime.datetime.now())
+          with open(featSelectFilename, 'wb') as featSelectF:
+              pickle.dump(self.featSelect, featSelectF)
+      else:
+          with open("featselect_2016-04-22 15:25:06.304474.pkl", 'rb') as featselectF:
+              self.featSelect = pickle.load(featselectF)
       self.model.fit(X, y)
+      #self.model.fit(X,y)
 
+    def printSelectedFeats(self):
+      featIndxs = [i for i,x in enumerate(self.featSelect.get_support()) if x == True]
+      for feat, indx in self.vec.vocabulary_.items():
+        if indx in featIndxs:
+          print("Selected feature:{0}".format(feat))
     def predict(self, article, feats):
       features = self.extract_features(article, feats)
       f = self.vec.transform(features).toarray()
-      prediction = self.model.predict(f)
-      prob = self.model.predict_proba(f)
-      return self.model.predict(f)[0]
+      #prediction = self.model.predict(f)
+      #prob = self.model.predict_proba(f)
+      f = self.featSelect.transform(f)
+      return int(self.model.predict(f)[0])
 
 def main():
   train_data = open('trainingSet.dat', 'r').read()
@@ -73,8 +106,9 @@ def main():
     feats = devSyntaxFeats[i]
 
     pred = model.predict(dev_data[i], feats)
-    if pred == dev_labels[i]:
+    if pred == int(dev_labels[i]):
       correct_preds += 1
-  print "model accuracy:", float(correct_preds)/len(dev_labels)
+  print ("model accuracy:", float(correct_preds)/len(dev_labels))
 
 main()
+#trainSyntax.generate()
