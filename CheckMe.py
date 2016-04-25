5a6df17ece9fa57f9b76473f9d6f244dd1ac1535
elliotschumacher@gmail.com <elliotschumacher@gmail.com>
2016-04-25 13:56:58 -0400

    Updated checkme for ll

--- CheckMe.py
+++ CheckMe.py
@@ -3,16 +3,22 @@ Created on Apr 24, 2016
 
 @author: elliotschumacher
 '''
+import math
 import sys
 def check():
     with open(sys.argv[1], "r") as predfile, open(sys.argv[2], "r") as actualfile:
         predlines = predfile.readlines()
         actlines = actualfile.readlines()
         correct = 0.0
+        loglikelihood = 0
+        
         for i in range(0, len(actlines)):
             if int(predlines[i].split()[2].strip()) == int(actlines[i].strip()):
                 correct += 1.0
+            loglikelihood += math.log(float(predlines[i].split()[int(actlines[i].strip())]))
+        
         print(correct / len(actlines))
+        print(loglikelihood / len(actlines))
     pass
 
 
